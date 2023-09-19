e0abbc716717f33a6bcffbd9ac1135c6eb6cb4da
Ajao Victor Oluwatimileyin <victoroluwatimileyin3@gmail.com>
2023-09-19 16:06:06 +0000

    all set

---
+++ script.js
@@ -0,0 +1,31 @@
+function one() {
+    document.getElementById("swipes").style.backgroundColor = "blue"
+    document.getElementById("swipes").style.backgroundImage = "url(./news.jpg)"
+}
+function two() {
+    document.getElementById("swipes").style.backgroundImage = "url(./pink.jpg)"
+}
+// ./pink\ lady.jpg
+function three() {
+    // document.getElementById("swipes").style.backgroundColor = "brown"
+    document.getElementById("swipes").style.backgroundImage = "url(./player.jpg)"
+   
+}
+function popular() {
+    alert(`please sign in `)
+    document.getElementById("file").href = "login.html"
+}
+function lesspopular() {
+    alert(`please sign in `)
+    document.getElementById("fil").href = "login.html"
+}
+function gethulu() {
+    document.getElementById("get").href = "signup.html"
+}
+function duo() {
+    document.getElementById("du").href = "signup.html"
+}
+function trio() {
+    document.getElementById("tri").href = "signup.html"
+}
+
---
+++ signup.js
@@ -0,0 +1,141 @@
+function line() {
+    document.getElementById("one").innerHTML = "Get Hulu 30 days free"
+    document.getElementById("two").innerHTML = "then $7.99/month"
+    document.getElementById("three").innerHTML = "Our Hulu (With Ads) plan lets you watch exclusive series, hit movies, Originals, kids shows, and tons more."
+    document.getElementById("five").innerHTML = "Looking for our student discount?"
+    document.getElementById("imgg").src = ""
+    document.getElementById("jan").style.border = "1px solid brown"
+    document.getElementById("feb").style.border = "none"
+    document.getElementById("mar").style.border = "none"
+    document.getElementById("apr").style.border = "none"
+    document.getElementById("may").style.border = "none"
+    document.getElementById("jun").style.border = "none"
+    document.getElementById("a").src = "./signup.img/tick.jpg"
+    document.getElementById("b").src = ""
+    document.getElementById("c").src = ""
+    document.getElementById("d").src = ""
+    document.getElementById("e").src = ""
+    document.getElementById("f").src = ""
+}
+function love() {
+    document.getElementById("imgg").src = "./signup.img/one.svg"
+    document.getElementById("one").style.display = "none"
+    document.getElementById("two").innerHTML = "Get Disney Bundle Duo Basic for $9.99/month."
+    document.getElementById("three").innerHTML = "Access endless entertainment with Disney+ (With Ads), and award-winning Hulu Originals with Hulu (With Ads)."
+    document.getElementById("five").innerHTML = "Learn More"
+    document.getElementById("feb").style.border = "1px solid brown"
+    document.getElementById("jan").style.border = "none"
+    document.getElementById("mar").style.border = "none"
+    document.getElementById("apr").style.border = "none"
+    document.getElementById("may").style.border = "none"
+    document.getElementById("jun").style.border = "none"
+    document.getElementById("b").src = "./signup.img/tick.jpg"
+    document.getElementById("a").src = ""
+    document.getElementById("c").src = ""
+    document.getElementById("d").src = ""
+    document.getElementById("e").src = ""
+    document.getElementById("f").src = ""
+}
+function live() {
+    document.getElementById("imgg").src = "./signup.img/two.svg"
+    document.getElementById("one").style.display = "none"
+    document.getElementById("two").innerHTML = "Get Disney Bundle Trio Basic for $12.99/month*."
+    document.getElementById("three").innerHTML = "Access live sports with ESPN+ (With Ads), endless entertainment with Disney+ (With Ads), and award-winning Hulu Originals with Hulu (With Ads). *Savings compared to regular monthly price of each service."
+    document.getElementById("four").innerHTML = "*Price will increase to $14.99/month on 10/12/2023."
+    document.getElementById("five").innerHTML = "Learn More"
+    document.getElementById("mar").style.border = "1px solid brown"
+
+    document.getElementById("jan").style.border = "none"
+    document.getElementById("feb").style.border = "none"
+    document.getElementById("apr").style.border = "none"
+    document.getElementById("may").style.border = "none"
+    document.getElementById("jun").style.border = "none"
+    document.getElementById("c").src = "./signup.img/tick.jpg"
+    document.getElementById("b").src = ""
+    document.getElementById("a").src = ""
+    document.getElementById("d").src = ""
+    document.getElementById("e").src = ""
+    document.getElementById("f").src = ""
+}
+function life() {
+    document.getElementById("imgg").src = "./signup.img/two.svg"
+    document.getElementById("one").style.display = "none"
+    document.getElementById("two").innerHTML = "Get Disney Bundle Trio Premium for $19.99/month*."
+    document.getElementById("three").innerHTML = "Access live sports with ESPN+ (With Ads), endless entertainment with Disney+ (No Ads), and award-winning Hulu Originals with Hulu (No Ads)."
+    document.getElementById("four").innerHTML = "*Price will increase to $24.99/month on 10/12/2023."
+    document.getElementById("five").innerHTML = "learn More"
+    document.getElementById("apr").style.border = "1px solid brown"
+    document.getElementById("jan").style.border = "none"
+    document.getElementById("feb").style.border = "none"
+    document.getElementById("mar").style.border = "none"
+    document.getElementById("may").style.border = "none"
+    document.getElementById("jun").style.border = "none"
+    document.getElementById("d").src = "./signup.img/tick.jpg"
+    document.getElementById("b").src = ""
+    document.getElementById("c").src = ""
+    document.getElementById("a").src = ""
+    document.getElementById("e").src = ""
+    document.getElementById("f").src = ""
+}
+
+function leap() {
+    document.getElementById("imgg").src = ""
+    document.getElementById("one").innerHTML = "$49.99/month for 3 mos., then regular monthly price*"
+    document.getElementById("two").innerHTML = "Access 90+ channels with Live TV, Unlimited DVR, endless entertainment with Disney+ (With Ads), live sports with ESPN+ (With Ads), and award-winning Hulu originals with Hulu (With Ads)."
+    document.getElementById("three").innerHTML = "*Price will increase to $76.99/month on 10/12/2023."
+    document.getElementById("five").innerHTML = "Learn More"
+    document.getElementById("six").innerHTML = "*Auto-renews at $76.99/month at the expiration of 3-month promotional period (if applicable)."
+    document.getElementById("may").style.border = "1px solid brown"
+
+    document.getElementById("jan").style.border = "none"
+    document.getElementById("feb").style.border = "none"
+    document.getElementById("mar").style.border = "none"
+    document.getElementById("apr").style.border = "none"
+    document.getElementById("jun").style.border = "none"
+    document.getElementById("e").src = "./signup.img/tick.jpg"
+    document.getElementById("b").src = ""
+    document.getElementById("c").src = ""
+    document.getElementById("d").src = ""
+    document.getElementById("a").src = ""
+    document.getElementById("f").src = ""
+}
+function leen() {
+    document.getElementById("imgg").src = ""
+    document.getElementById("one").innerHTML = "Get Hulu (No Ads) 30 days free"
+    document.getElementById("two").innerHTML = "then $14.99/month*"
+    document.getElementById("three").innerHTML = "Our Hulu (No Ads) plan lets you watch exclusive series, hit movies, Originals, kids shows, and tons more with no ad interruptions."
+    document.getElementById("four").innerHTML = "*Price will increase to $17.99/month on 10/12/2023."
+    document.getElementById("jun").style.border = "1px solid brown"
+    document.getElementById("jan").style.border = "none"
+    document.getElementById("feb").style.border = "none"
+    document.getElementById("mar").style.border = "none"
+    document.getElementById("apr").style.border = "none"
+    document.getElementById("may").style.border = "none"
+    document.getElementById("f").src = "./signup.img/tick.jpg"
+    document.getElementById("b").src = ""
+    document.getElementById("c").src = ""
+    document.getElementById("d").src = ""
+    document.getElementById("e").src = ""
+    document.getElementById("a").src = ""
+}
+function smalbutton() {
+    document.getElementById("plus").href = "signin.html"
+}
+function one() {
+    document.getElementById("go").href = "signin.html"
+}
+function two() {
+    document.getElementById("come").href = "signin.html"
+}
+function three() {
+    document.getElementById("lin").href = "signin.html"
+}
+function four() {
+    document.getElementById("lip").href = "signin.html"
+}
+function five() {
+    document.getElementById("lid").href = "signin.html"
+}
+function six() {
+    document.getElementById("lit").href = "signin.html"
+}
